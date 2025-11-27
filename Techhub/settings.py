a42e852db84d8ed0497ff151ca20f98b49084a66
georgebrian7 <brianmecha021@gmail.com>
2025-11-27 18:19:22 +0300

    <fix deployment>

--- Techhub/settings.py
+++ Techhub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$!63jsf0bbt1ed1lw*l*wfj4#vhjora=*+kw-0ky8frwlhr)uh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'techhubpage.onrender.com']
 
 
 # Application definition
