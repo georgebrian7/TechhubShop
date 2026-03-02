06c48de1ac0d1473802f0feaa682c65c856f5b45
georgebrian7 <brianmecha021@gmail.com>
2026-03-02 11:52:58 +0300

    <fix><environment variable issue>

--- Techhub/settings.py
+++ Techhub/settings.py
@@ -201,7 +201,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=2525, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='TechHub <noreply@techhub.com>')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Keshilimited <noreply@keshilimited.com>')
 
 
 # EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
