222c6df0495594dd95f7d01ff3331f6aa27a0435
georgebrian7 <brianmecha021@gmail.com>
2026-03-02 11:41:10 +0300

    <feat><finished intergration of mailtrap email>

--- Techhub/settings.py
+++ Techhub/settings.py
@@ -195,6 +195,13 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Looking to send emails in production? Check out our Email API/SMTP product!
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='sandbox.smtp.mailtrap.io')
+EMAIL_PORT = config('EMAIL_PORT', default=2525, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='TechHub <noreply@techhub.com>')
 
 
 # EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
